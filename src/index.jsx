0b4d3158d3420073417e2742879fec8334f1c664
Manvi Huyen <manvi.huyen@gmail.com>
2016-06-22 22:28:45 -0700

    new file

--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,12 @@
 var React = require('react');
 var ReactDom = require('react-dom');
+
+var Message = React.createClass ({
+    render: function() {
+      return (
+          <p> Let it go </p>
+      );
+    }
+});
+
+ReactDom.render (<Message />, document.getElementById('app'));
